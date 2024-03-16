99f28b34a4bb5c4ff8279102712dc4815d18dc10
saurav1812 <shorya181299@gmail.com>
2024-03-16 18:46:48 +0530

    normal commit

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 4518;
+export const mongoDBURL = `mongodb://localhost:27017`
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
 
 const app = express();
 
-app.get('/',(req,res)=>{
-            console.log(req);
-            return res.status(234).send("Hey Saurav You are Good to Go");
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Hey Saurav You are Good to Go");
 });
 
 app.listen(PORT, () => {
-    console.log(`App is listing to port : ${PORT}`);
-  });
+  console.log(`App is listing to port : ${PORT}`);
+});
+
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {})
+  .catch(() => {});
