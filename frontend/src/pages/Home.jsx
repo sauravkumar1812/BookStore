b0fd7a55fb060c7dbe5be764e0b6337b0b225ee8
saurav1812 <shorya181299@gmail.com>
2024-03-19 23:56:58 +0530

    final commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import {AiOutlineEdit} from 'react-icons/ai';
 import {BsInfoCircle} from 'react-icons/bs'
 import {MdOutlineAddBox,MdOutlineDelete} from 'react-icons/md'
 
+
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -15,6 +16,7 @@ const Home = () => {
       .get("http://localhost:4518/books")
       .then((res) => {
         setBooks(res.data.data);
+        
         setLoading(false);
       })
       .catch((error) => {
@@ -44,8 +46,9 @@ const Home = () => {
           </tr>
           </thead>
           <tbody>
-            {books.map ((book,index) => (
+             {books?.map((book,index) => (
                 <tr key = {book.id} className="h-8">
+                    
                       <td className="border border-slate-700 rounded-md text-center">
                         {index+1}
                       </td>
@@ -53,7 +56,7 @@ const Home = () => {
                         {book.title}
                       </td>
                       <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                        {book.author}
+                    {book.author}
                       </td>
                       <td className="border border-slate-700 rounded-md text-center max-md:hidden">
                         {book.publishYear}
@@ -75,7 +78,6 @@ const Home = () => {
             ))}
           </tbody>
      </table>
-
       )}
     </div>
   );
