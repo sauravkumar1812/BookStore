2ad00201c3fecb9862e92c103496f35da73670c2
saurav1812 <shorya181299@gmail.com>
2024-05-15 01:29:59 +0530

    normal commit

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import Backbutton from "../components/button";
-import { response } from "express";
-import BackButton from "../components/button";
+import BackButton from "../components/button"; 
+import Spinner from "../components/Spinner";
 
 const ShowBook = () => {
   const [book, setBook] = useState({});
-  const [loadind, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const { id } = useParams();
 
   useEffect(() => {
@@ -22,42 +21,44 @@ const ShowBook = () => {
         console.log(error);
         setLoading(false);
       });
-  }, [id]);
-  return <div className="p-4 ">
-    <BackButton/>
-    <h1 className="text-3xl my-4">Show Book</h1>
-    {
-      loading ? (
-        <spinner/>
-      ) :(
+  }, []); 
+  return (
+    <div className="p-4">
+      <BackButton/>
+      <h1 className="text-3xl my-4">Show Book</h1>
+      {loading ? (
+        <Spinner/>
+      ) : (
         <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
           <div className="my-4">
-              <span className="text-tl mr-4 text-gray-500">Id</span>
-              <span>{book._id}</span>
-            </div> 
-            <div className="my-4">
-              <span className="text-tl mr-4 text-gray-500">Tittle</span>
-              <span>{book.tittle}</span>
-            </div> 
-            <div className="my-4">
-              <span className="text-tl mr-4 text-gray-500">Author</span>
-              <span>{book.author}</span>
-            </div> 
-            <div className="my-4">
-              <span className="text-tl mr-4 text-gray-500">Publish Year</span>
-              <span>{book.publishyear}</span>
-            </div> 
-            <div className="my-4">
-              <span className="text-tl mr-4 text-gray-500">Create Time</span>
-              <span>{new Date(Book.createAt).toString()}</span>
-            </div> 
-            <div className="my-4">
-              <span className="text-tl mr-4 text-gray-500">Last Upadte Time</span>
-              <span>{new Date(Book.updatedAt).toString()}</span>
-            </div> 
-          </div>
+            <span className="text-tl mr-4 text-gray-500">Id</span>
+            <span>{book._id}</span>
+          </div> 
+          <div className="my-4">
+            <span className="text-tl mr-4 text-gray-500">Title</span>
+            <span>{book.title}</span> 
+          </div> 
+          <div className="my-4">
+            <span className="text-tl mr-4 text-gray-500">Author</span>
+            <span>{book.author}</span>
+          </div> 
+          <div className="my-4">
+            <span className="text-tl mr-4 text-gray-500">Publish Year</span>
+            <span>{book.publishYear}</span> 
+          </div> 
+          <div className="my-4">
+            <span className="text-tl mr-4 text-gray-500">Create Time</span>
+            <span>{new Date(book.createdAt).toString()}</span> 
+          </div> 
+          <div className="my-4">
+            <span className="text-tl mr-4 text-gray-500">Last Update Time</span>
+            <span>{new Date(book.updatedAt).toString()}</span> 
+          </div> 
+        </div>
       )}
-  </div>;
+    </div>
+  );
 };
 
 export default ShowBook;
+
